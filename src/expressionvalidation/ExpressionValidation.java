0885f2e059af7dea85ecd131b384f847369a6bf3
GittinInThere <awalletsdream@gmail.com>
2019-09-04 13:51:10 +1200

    Update ExpressionValidation.java

--- src/expressionvalidation/ExpressionValidation.java
+++ src/expressionvalidation/ExpressionValidation.java
@@ -5,17 +5,61 @@
  */
 package expressionvalidation;
 
-/**
- *
- * @author awall
- */
+import java.util.Scanner;
+import java.util.Stack;
+
+
 public class ExpressionValidation {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome to the expression validating program.");
+        Scanner sc = new Scanner(System.in);
+        //Set up a scanner
+        
+        System.out.println("\nEnter your expression:");
+        String expression = sc.nextLine();
+        //Get the expression through user input
+        
+        System.out.println("\nEnter the opening character to validate:");
+        char openingChar = sc.nextLine().charAt(0);
+        //Pull the char out of the user input
+        
+        System.out.println("\nEnter the closing character to validate:");
+        char closingChar = sc.nextLine().charAt(0);
+        //Pull the char out of the user input
+        
+        Stack charStack = new Stack();
+        
+        for (int i = 0; i < expression.length(); i++) {
+            
+            if(expression.charAt(i) == openingChar){
+                //If there is a match, add an entry to the stack
+                charStack.push(expression.charAt(i));
+                
+            } else if(expression.charAt(i) == closingChar){
+                
+                if(!charStack.isEmpty()){
+                    charStack.pop();
+                    
+                } else {
+                    //Stack is empty and there is a closing char
+                    System.out.println("\nExpression invalid. Error:"
+                            + " Closing char");
+                    return;
+                }
+                
+            }  
+        }
+        
+        if(!charStack.isEmpty()){
+            //If the stack is not empty and all of the string has been checked
+            // - means there are too many opening chars
+            System.out.println("\nExpression invalid. Error: Opening char");
+            
+        } else {
+            System.out.println("\nThe expression is validated.");
+        }
     }
     
 }
